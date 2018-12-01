8196c2872f6920a8bf800a35433a59f703947138
David Lord <davidism@gmail.com>
2018-11-30 21:58:09 -0800

    2018 1 2

--- year2018/day01/solve.py
+++ year2018/day01/solve.py
@@ -1,4 +1,18 @@
+from itertools import cycle
+
 from common import read_lines
 
-data = read_lines()
-print(sum(int(x) for x in data))
+data = [int(x) for x in read_lines()]
+print(sum(x for x in data))
+
+seen = set()
+current = 0
+
+for item in cycle(data):
+    current += item
+
+    if current in seen:
+        print(current)
+        break
+
+    seen.add(current)

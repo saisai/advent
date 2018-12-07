780d635647d192bef439ca8bcc96c768afd7aea2
David Lord <davidism@gmail.com>
2018-12-07 10:15:24 -0800

    2018 7 2

--- year2018/day07/solve.py
+++ year2018/day07/solve.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+from bisect import insort_left
 from networkx import DiGraph
 from networkx import lexicographical_topological_sort
 
@@ -10,4 +13,35 @@ def parse(lines):
     )
 
 
-print("".join(lexicographical_topological_sort(parse(read_lines()))))
+graph = parse(read_lines())
+print("".join(lexicographical_topological_sort(graph)))
+
+
+def build(graph, workers=5, base_cost=60):
+    base_cost -= ord("A") - 1
+    incomplete = set(graph.nodes)
+    available = sorted(n for n, d in graph.in_degree if d == 0)
+    schedule = defaultdict(list)
+    time = 0
+
+    while incomplete:
+        done = schedule.pop(time, ())
+        incomplete.difference_update(done)
+        workers += len(done)
+
+        for done_node in done:
+            for next_node in graph.successors(done_node):
+                if not set(graph.predecessors(next_node)).intersection(incomplete):
+                    insort_left(available, next_node)
+
+        while workers and available:
+            node = available.pop(0)
+            workers -= 1
+            schedule[time + base_cost + ord(node)].append(node)
+
+        time = min(schedule.keys(), default=time)
+
+    return time
+
+
+print(build(graph))

bf2f21d529cb5af430d9cf94f0821537fe47f2ed
David Lord <davidism@gmail.com>
2018-12-15 13:57:13 -0800

    2018 14 2

--- year2018/day14/solve.py
+++ year2018/day14/solve.py
@@ -1,26 +1,44 @@
-def run(improve):
-    cutoff = improve + 10
+def run(puzzle):
+    improve = None
+    improve_start = int(puzzle)
+    improve_end = improve_start + 10
+    appear = None
+    digits = tuple(int(c) for c in puzzle)
+    digit_gen = iter(digits)
     board = [3, 7]
-    bl = 2
+    board_length = 2
     e0p = 0
     e1p = 1
 
-    while bl < cutoff:
+    while improve is None or appear is None:
         e0s = board[e0p]
         e1s = board[e1p]
         combine = e0s + e1s
 
         if combine < 10:
-            board.append(combine)
-            bl += 1
+            values = (combine,)
         else:
-            board.extend(divmod(combine, 10))
-            bl += 2
+            values = divmod(combine, 10)
 
-        e0p = (e0p + e0s + 1) % bl
-        e1p = (e1p + e1s + 1) % bl
+        for x in values:
+            n = next(digit_gen, None)
 
-    print("".join(str(x) for x in board[improve : improve + 10]))
+            if n is None:
+                appear = board_length - len(digits)
+            elif n != x:
+                digit_gen = iter(digits)
+
+            board.append(x)
+            board_length += 1
+
+        e0p = (e0p + e0s + 1) % board_length
+        e1p = (e1p + e1s + 1) % board_length
+
+        if improve is None and board_length >= improve_end:
+            improve = "".join(str(x) for x in board[improve_start:improve_end])
+
+    print(improve)
+    print(appear)
 
 
-run(880_751)
+run("880751")
